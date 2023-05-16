fa7d0a70f90ddc3948b075d6fdc00877faf3cd4d
augustolfp <augustolfp@gmail.com>
2023-05-16 17:13:27 -0300

    feat(data fetching): implement GET request for recipe list

--- src/components/RecipesPage.tsx
+++ src/components/RecipesPage.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { api } from "../config/axios";
+
 export default function RecipesPage() {
+    type Recipe = {
+        titulo: string;
+        receita: string;
+    };
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<Recipe[] | []>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await api.get("/recipes");
+                setData(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+            setLoading(false);
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <div>This is the secret recipes page!</div>
+            {loading && <div>Getting recipes from the API...</div>}
+            {!loading && (
+                <div>
+                    <h2>Found some recipes:</h2>
+                    {data.map((recipe, index) => (
+                        <div key={index}>
+                            <h3>Title: {recipe.titulo}</h3>
+                            <h3>Recipe: {recipe.receita}</h3>
+                        </div>
+                    ))}
+                </div>
+            )}
         </>
     );
 }
